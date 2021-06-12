ed2e34608d68013f558d188b92dd5963473d0ceb
Daniel Hoang <danielvanhoang@gmail.com>
2021-06-11 22:18:15 -0500

    same

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 import time
+import beeline
+
+beeline.init(
+    writekey='1252c1ce92b00ad574131fa6e873366d',
+    dataset='cheems-app',
+    service_name='cheems-app',
+    debug=False, # defaults to False. if True, data doesn't get sent to Honeycomb
+)
 
 i=0;
 while(i < 100):
